8326864c1db616f546ca902937f5906417d33faf
sanjay20yadav <yadav.sanjay20yadav@gmail.com>
2020-07-14 09:23:29 +0530

    message

---
+++ man/Yadav.php
@@ -0,0 +1,14 @@
+<?php
+namespace Mansh;
+class Yadav {
+//
+ public $firstName='Mansh';
+ public $lastName ='Yadav';
+ //
+ public function name () :string 
+ {
+     return $this->firstName.' '.$this->lastName;
+ }
+
+}
+?>
